abcea05986748278becddcd733e39874c8fa042b
sandeepnarhe <sandeepnarhe@gmail.com>
2020-04-06 16:37:22 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("This is Sandeep Narhe")
+const express = require("express")
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/",(request,response)=>{
+    
+    response.send(`<h1> Sandeep Narhe </h1> ${PORT}`);
+});
+
+app.listen(PORT,()=> console.log(`Server is running on PORT : ${PORT}`))
+
